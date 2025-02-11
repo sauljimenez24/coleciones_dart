574ef25130eb4c6e4e5cb766e9c230e1984233b7
sauljimenez24 <a.22308051281222@cbtis128.edu.mx>
2025-02-11 16:53:58 +0000

    hola lista

---
+++ lista1.dart
@@ -0,0 +1,12 @@
+void main() {  
+ List<int>numeros =[10, 20,30];
+ print("Lista de numeros enteros :");
+ print(numeros);
+ print("primer elemento : ${numeros[0]}");
+ print("segundo elemento : ${numeros[1]}");
+ print("tercer elemento : ${numeros[2]}");
+ print("Elementos de la lista usando ciclo for:");
+ for(int i=0;  i <numeros.length; i++){
+   print(numeros[i]);
+ }
+}  
